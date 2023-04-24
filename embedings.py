2b1b9a4233a47db62752efc6b3e82eaf11ddbbea
raphael_campus <raphael.bouvet@le-campus-numerique.fr>
2023-04-24 16:37:19 +0200

    update embedding to add tqdm progress bar

--- embedings.py
+++ embedings.py
@@ -3,40 +3,35 @@ from pathlib import Path
 import pandas as pd
 import torch
 from transformers import AutoFeatureExtractor, AutoModel
+from tqdm import tqdm
 
-
-model_ckpt = 'google/vit-base-patch16-224-in21k'
+model_ckpt = 'microsoft/swin-tiny-patch4-window7-224'
 extractor = AutoFeatureExtractor.from_pretrained(model_ckpt)
 model = AutoModel.from_pretrained(model_ckpt)
 data = []
 index = []
 path = Path('data/img')
 
-for file in path.rglob('*.jpg'):
-    print(file)
+for file in tqdm(path.rglob('*.jpg')):
+    # print(file)
     image = Image.open(file)
     try:
         inputs = extractor(image, return_tensors="pt")
-        print(inputs['pixel_values'].shape)
+        # print(inputs['pixel_values'].shape)
         with torch.no_grad():
             outputs = model(**inputs)
         # print(outputs)
         last_hidden_states = outputs.last_hidden_state[0, 0, :].cpu().numpy()
-        print(last_hidden_states.shape)
+        # print(last_hidden_states.shape)
         data.append(last_hidden_states)
         index.append(file.stem)
     except ValueError:
         continue
 
-   
-
 df = pd.DataFrame(data)
 df.index = index
 print(df.head())
-df.to_csv('data/embedding_vit.csv')
-
-
-# listing all image files 
+df.to_csv('data/embedding_swin.csv')
 
 
 
