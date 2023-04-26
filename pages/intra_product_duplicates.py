34c02fac1b4588d6810396edd1c99dd8a1e04d52
raphael_campus <raphael.bouvet@le-campus-numerique.fr>
2023-04-26 09:52:22 +0200

    now can be used to look at images by categories
    to do:
    for each image show only pairs of images that are similar
    either show the closest or use a threshold

--- pages/intra_product_duplicates.py
+++ pages/intra_product_duplicates.py
@@ -1,40 +1,42 @@
 import streamlit as st
 from PIL import Image
 
+if 'counter_categories' not in st.session_state: 
+    st.session_state['counter_categories'] = 0
+
 images = st.session_state['image_list']
 product_data = st.session_state['product_data']
 cosines = st.session_state['cosines']
-categories = [image.stem.split('_')[0] for image in images]
-categories = set(categories)
+images_embedding = st.session_state['embedding_data'].reset_index()
+images_embedding['categories'] = images_embedding['image_name'].apply(lambda x: x.split('_')[0])
+images_embedding = images_embedding.set_index('categories')
 
-choice_image = st.selectbox(label='Select an image', options=images, index=0)
-if choice_image:
-    categorie = choice_image.stem.split('_')[0]
-    product_name = product_data[categorie]['product_name']
-    
-    st.write(f'Selected product name : {product_name}')
-    st.write(f'Selected categorie : {categorie}')
-    image = Image.open(choice_image)
-    st.image(image)
+categories = sorted(set(images_embedding.index))
+# choice_image = st.selectbox(label='Select an image', options=images, index=0)
 
-    st.header('Visualisation of close images')
-    # st.dataframe(embedding_data.head())
-    # st.dataframe(embedding_data.loc[choice_image.stem])
-    closest_to_query = cosines.loc[choice_image.stem].sort_values()
-    # remove if categorie is different not similar
-    closest_to_query.name = 'cosine_distance'
-    closest_to_query = closest_to_query.reset_index()
-    closest_to_query['categories'] = closest_to_query['image_name'].apply(lambda x: x.split('_')[0])
-    closest_to_query = closest_to_query.set_index('categories').loc[categorie]
-    # st.dataframe(closest_to_query)
+choice_category = categories[st.session_state['counter_categories']]
+next = st.button('Next category')
+previous = None
+if st.session_state['counter_categories'] > 0:
+    previous = st.button('Previous Category')
+if next:
+    st.session_state['counter_categories'] += 1
+if previous:
+    st.session_state['counter_categories'] -= 1
 
-    images = []
-    captions = []
-    for row in closest_to_query.itertuples():
+
+st.write(f'{st.session_state["counter_categories"]}')
+product_name = product_data[choice_category]['product_name']
+st.write(f'Product name : {product_name}  Category id : {choice_category}')
+
+# Get all images from the selected category
+
+all_images_of_category = images_embedding.loc[choice_category][['image_name']]
+
+images = []
+if len(all_images_of_category) > 1:
+    for row in all_images_of_category.sort_values('image_name').itertuples():
         images.append(Image.open(f'data/img/{row.image_name}.jpg'))
-        # product_name = product_data[row.Index]['product_name']
-        captions.append(f'{row.cosine_distance}')
-    st.image(images, captions)
-    
-
-
+else:
+    images.append(Image.open(f'data/img/{all_images_of_category["image_name"]}.jpg'))
+st.image(images)
